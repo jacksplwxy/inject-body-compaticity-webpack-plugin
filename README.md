1de2891be45f9d39ab12a03e8e00505488803ba9
王兴宇 <wangxingyu4@xiamenair.com>
2022-05-22 13:00:14 +0800

    feat：initial version

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# inject-body-compaticity-webpack-plugin
-Webpack plugin that injects a custom string into the body of the html-webpack-plugin output.It is compatible with different versions of WebPack and HtmlWebPackPlugin
+# inject-body-compaticity-webpack-plugin Introduction
+A [HTML Webpack Plugin](https://github.com/jacksplwxy/html-webpack-plugin)  that injects a custom string into the body of the html-webpack-plugin output.It is compatible with different versions of WebPack and HtmlWebPackPlugin. (inspired by [inject-body-webpack-plugin](https://github.com/Jaid/inject-body-webpack-plugin))
+
+
+<br>
+
+# Differences with inject-body-webpack-plugin
+* It is compatible with different versions of WebPack and HtmlWebPackPlugin.
+* It can work according to process.env.NODE_ENV.
+
+<br>
+
+# Installation
+
+```bash
+npm i -D inject-body-compaticity-webpack-plugin
+```
+
+<br>
+
+# Example
+
+### Input
+
+**webpack.config.js**
+
+```js
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import InjectBodyCompaticityWebpackPlugin from "inject-body-compaticity-webpack-plugin"
+
+export default {
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new InjectBodyCompaticityWebpackPlugin({
+      position:'start',
+      content: '<script>alert("Hello InjectBodyCompaticityWebpackPlugin!")</script>',
+      env: ["development"]
+    }),
+  ],
+}
+```
+
+
+### Output
+
+**index.html**
+
+```html
+<html><body><script>alert("Hello InjectBodyCompaticityWebpackPlugin!")</script></body></html>
+```
+
+
+<br>
+
+# Options
+<table>
+<tr>
+<th></th>
+<th>Type</th>
+<th>Default</th>
+<th>Info</th>
+</tr>
+<tr>
+<td>position</td>
+<td>string</td>
+<td>start</td>
+<td>If “start”, the content will be injected as close to the body opening tag as possible. If “end”, the content will be injected as close to the body ending tag as possible.</td>
+</tr>
+<tr>
+<td>content</td>
+<td>string</td>
+<td>""</td>
+<td>The text that will be injected into the final HTML output.</td>
+</tr>
+<tr>
+<td>env</td>
+<td>string[]</td>
+<td>[]</td>
+<td>When the value of process.env.NODE_ENV is in the list, the plugin will take effect</td>
+</tr>
+</table>
+
---
+++ src/main.ts
@@ -0,0 +1,146 @@
+import type { default as HtmlWebpackPluginInstance } from "html-webpack-plugin";
+import type { Compiler, WebpackPluginInstance } from "webpack";
+import debug from "./debug";
+import { PLUGIN_NAME } from "./const";
+
+export type Env = string | undefined;
+
+export interface Options {
+  position?: string;
+  content?: string;
+  env?: Env[];
+}
+
+const DEFAULT_OPTIONS: Options = {
+  position: "start",
+  content: "",
+  env: [],
+};
+
+export default class EnvInjectBodyPlugin implements WebpackPluginInstance {
+  private options: Options = DEFAULT_OPTIONS;
+
+  constructor(options: Options) {
+    this.options = {
+      ...this.options,
+      ...options,
+    };
+  }
+
+  /**
+   * Extract HTMLWebpack Plugin
+   *
+   * @param compiler
+   */
+  private extractHtmlWebpackPluginModule = (
+    compiler: Compiler
+  ): typeof HtmlWebpackPluginInstance | null => {
+    const htmlWebpackPlugin = (compiler.options.plugins || []).find(
+      (plugin) => {
+        return plugin.constructor.name === "HtmlWebpackPlugin";
+      }
+    ) as typeof HtmlWebpackPluginInstance | undefined;
+    if (!htmlWebpackPlugin) {
+      return null;
+    }
+    const HtmlWebpackPlugin = htmlWebpackPlugin.constructor;
+    if (!HtmlWebpackPlugin) {
+      return null;
+    }
+    return HtmlWebpackPlugin as typeof HtmlWebpackPluginInstance;
+  };
+
+  apply(compiler: Compiler) {
+    const acceptEnv = this.options.env;
+    if (!Array.isArray(acceptEnv) || acceptEnv.length === 0) {
+      debug("the env option is not specified, the plugin does nothing");
+      return;
+    }
+    if (!acceptEnv.includes(process.env.NODE_ENV!)) {
+      debug(
+        "%s needs to be one of the following values: %o",
+        "process.env.NODE_ENV",
+        acceptEnv
+      );
+      debug("does nothing");
+      return;
+    }
+
+    const HtmlWebpackPlugin = this.extractHtmlWebpackPluginModule(compiler);
+    if (!HtmlWebpackPlugin) {
+      throw new Error(
+        `${PLUGIN_NAME} needs to be used with html-webpack-plugin`
+      );
+    }
+
+    if (compiler.hooks) {
+      // webpack 4 support
+      compiler.hooks.compilation.tap(PLUGIN_NAME, (compilation) => {
+        // @ts-ignore
+        if (compilation.hooks.htmlWebpackPluginAfterHtmlProcessing) {
+          // @ts-ignore
+          compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
+            PLUGIN_NAME,
+            (htmlPluginData, callback) => {
+              if (this.options.position === "end") {
+                htmlPluginData.html = htmlPluginData.html.replace(
+                  "</body>",
+                  this.options.content + "</body>"
+                );
+              }else{
+                htmlPluginData.html = htmlPluginData.html.replace(
+                  "<body>",
+                  "<body>"+this.options.content
+                );
+              }
+              callback(null, htmlPluginData);
+            }
+          );
+        } else {
+          // HtmlWebPackPlugin 4.x
+          const hooks = HtmlWebpackPlugin.getHooks(compilation);
+          hooks.beforeEmit.tapAsync(PLUGIN_NAME, (htmlPluginData, callback) => {
+            try {
+              if (this.options.position === "end") {
+                htmlPluginData.html = htmlPluginData.html.replace(
+                  "</body>",
+                  this.options.content + "</body>"
+                );
+              }else{
+                htmlPluginData.html = htmlPluginData.html.replace(
+                  "<body>",
+                  "<body>"+this.options.content
+                );
+              }
+              callback(null, htmlPluginData);
+            } catch (error) {
+              callback(error as Error);
+            }
+          });
+        }
+      });
+    } else {
+      // webpack 3 support
+      // @ts-ignore
+      compiler.hooks("compilation", (compilation) => {
+        compilation.plugin(
+          "html-webpack-plugin-after-html-processing",
+          (htmlPluginData, callback) => {
+            if (this.options.position === "end") {
+              htmlPluginData.html = htmlPluginData.html.replace(
+                "</body>",
+                this.options.content + "</body>"
+              );
+            }else{
+              htmlPluginData.html = htmlPluginData.html.replace(
+                "<body>",
+                "<body>"+this.options.content
+              );
+            }
+            callback(null, htmlPluginData);
+          }
+        );
+      });
+    }
+  }
+}
